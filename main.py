3a818f51cc288198cb1bd44c02ba810e6c180dae
jukkakansanaho <jukka.kansanaho@gmail.com>
2024-12-04 06:50:59 +0200

    feat: Add game clock.

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ def main():
     pg.init()
     color = (0, 0, 0) # black
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    run = True
-    while run:
+    clock = pg.time.Clock()
+    dt = 0
+
+    running = True
+    while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-                #pg.quit()
-                #sys.exist()
+                
         screen.fill(color)
         pg.display.flip()
+        
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
