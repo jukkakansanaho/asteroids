becd4778eeb1fb7ca7caf341634de103258c722b
jukkakansanaho <jukka.kansanaho@gmail.com>
2024-12-07 07:57:25 +0200

    chore: Removing some print lines

--- asteroid.py
+++ asteroid.py
@@ -16,7 +16,6 @@ class Asteroid(CircleShape):
         self.position += (self.velocity * dt)
     
     def split(self):
-        #print(f"Asteroid_old (pos,rad,velo): {self.position},{self.radius},{self.velocity}")
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
@@ -30,5 +29,3 @@ class Asteroid(CircleShape):
         asteroid1.velocity = vect1 * 1.2
         asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid2.velocity = vect2 * 1.2
-        #print(f"Asteroid_new_1 (pos,rad,velo): {asteroid1.position},{asteroid1.radius},{asteroid1.velocity}")
-        #print(f"Asteroid_new_2 (pos,rad,velo): {asteroid2.position},{asteroid2.radius},{asteroid2.velocity}")
